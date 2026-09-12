74619043ede597e288e330d5eef1abf18133c3e9
sigurjonaxel <sigurjonaxel@users.noreply.github.com>
2026-09-12 01:04:14 +0000

    feat(avatars): add cache buster v=2 to all avatars and update encrypted payload

--- build_encrypted_pages.py
+++ build_encrypted_pages.py
@@ -51,16 +51,16 @@ def build():
     # Afmæli sótt beint úr Sag/Ancestry gagnagrunninum (ancestry.db)
     # https://sigurjonaxel.github.io/ancestry/
     family_birthdays = [
-        { "name": "Ólafía Rósbjörg (Lóa)", "shortName": "Lóa", "avatar": "images/avatars/loa.jpg", "date": "10. janúar 1974", "dayMonth": "10. jan", "year": 1974, "age2027": 53 },
-        { "name": "Vala Björk", "shortName": "Vala", "avatar": "images/avatars/vala.jpg", "date": "1. febrúar 2005", "dayMonth": "1. feb", "year": 2005, "age2027": 22 },
-        { "name": "Sigurjón Axel", "shortName": "Sigurjón", "avatar": "images/avatars/sigurjon.jpg", "date": "4. febrúar 1974", "dayMonth": "4. feb", "year": 1974, "age2027": 53 },
-        { "name": "Viktor Ingi", "shortName": "Viktor", "avatar": "images/avatars/viktor.jpg", "date": "14. mars 1999", "dayMonth": "14. mar", "year": 1999, "age2027": 28 },
-        { "name": "Birkir Evan", "shortName": "Birkir", "avatar": "images/avatars/birkir.jpg", "date": "6. maí 2014", "dayMonth": "6. maí", "year": 2014, "age2027": 13 },
-        { "name": "Axel Bjarkar", "shortName": "Axel", "avatar": "images/avatars/axel.jpg", "date": "22. júní 2003", "dayMonth": "22. jún", "year": 2003, "age2027": 24, "inSummer": True },
-        { "name": "Sara Kristín", "shortName": "Sara", "avatar": "images/avatars/sara.jpg", "date": "18. júlí 2010", "dayMonth": "18. júl", "year": 2010, "age2027": 17, "inSummer": True },
-        { "name": "Þórhildur Soffía", "shortName": "Þórhildur", "avatar": "images/avatars/thorhildur.jpg", "date": "10. ágúst 2010", "dayMonth": "10. ágú", "year": 2010, "age2027": 17, "inSummer": True },
-        { "name": "Rannveig Arna", "shortName": "Rannveig", "avatar": "images/avatars/rannveig.jpg", "date": "5. september 2005", "dayMonth": "5. sep", "year": 2005, "age2027": 22 },
-        { "name": "Anna Huyen Ngo", "shortName": "Anna", "avatar": "images/avatars/anna.jpg", "date": "2003", "dayMonth": "2003", "year": 2003, "age2027": 24 }
+        { "name": "Ólafía Rósbjörg (Lóa)", "shortName": "Lóa", "avatar": "images/avatars/loa.jpg?v=2", "date": "10. janúar 1974", "dayMonth": "10. jan", "year": 1974, "age2027": 53 },
+        { "name": "Vala Björk", "shortName": "Vala", "avatar": "images/avatars/vala.jpg?v=2", "date": "1. febrúar 2005", "dayMonth": "1. feb", "year": 2005, "age2027": 22 },
+        { "name": "Sigurjón Axel", "shortName": "Sigurjón", "avatar": "images/avatars/sigurjon.jpg?v=2", "date": "4. febrúar 1974", "dayMonth": "4. feb", "year": 1974, "age2027": 53 },
+        { "name": "Viktor Ingi", "shortName": "Viktor", "avatar": "images/avatars/viktor.jpg?v=2", "date": "14. mars 1999", "dayMonth": "14. mar", "year": 1999, "age2027": 28 },
+        { "name": "Birkir Evan", "shortName": "Birkir", "avatar": "images/avatars/birkir.jpg?v=2", "date": "6. maí 2014", "dayMonth": "6. maí", "year": 2014, "age2027": 13 },
+        { "name": "Axel Bjarkar", "shortName": "Axel", "avatar": "images/avatars/axel.jpg?v=2", "date": "22. júní 2003", "dayMonth": "22. jún", "year": 2003, "age2027": 24, "inSummer": True },
+        { "name": "Sara Kristín", "shortName": "Sara", "avatar": "images/avatars/sara.jpg?v=2", "date": "18. júlí 2010", "dayMonth": "18. júl", "year": 2010, "age2027": 17, "inSummer": True },
+        { "name": "Þórhildur Soffía", "shortName": "Þórhildur", "avatar": "images/avatars/thorhildur.jpg?v=2", "date": "10. ágúst 2010", "dayMonth": "10. ágú", "year": 2010, "age2027": 17, "inSummer": True },
+        { "name": "Rannveig Arna", "shortName": "Rannveig", "avatar": "images/avatars/rannveig.jpg?v=2", "date": "5. september 2005", "dayMonth": "5. sep", "year": 2005, "age2027": 22 },
+        { "name": "Anna Huyen Ngo", "shortName": "Anna", "avatar": "images/avatars/anna.jpg?v=2", "date": "2003", "dayMonth": "2003", "year": 2003, "age2027": 24 }
     ]
 
     weeks = [
