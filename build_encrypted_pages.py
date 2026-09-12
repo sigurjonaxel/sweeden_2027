5d543a802873f93cd5a98cd488ba399b0f8fd024
sigurjonaxel <sigurjonaxel@users.noreply.github.com>
2026-09-12 00:33:11 +0000

    Bæta við sænskum og dönskum fána á bláa bakgrunninn og tengja myndir úr Sag kerfinu við notendur og afmæli

--- build_encrypted_pages.py
+++ build_encrypted_pages.py
@@ -50,15 +50,15 @@ def build():
     # Afmæli sótt beint úr Sag/Ancestry gagnagrunninum (ancestry.db)
     # https://sigurjonaxel.github.io/ancestry/
     family_birthdays = [
-        { "name": "Ólafía Rósbjörg (Lóa)", "shortName": "Lóa", "date": "10. janúar 1974", "dayMonth": "10. jan", "year": 1974, "age2027": 53 },
-        { "name": "Vala Björk", "shortName": "Vala", "date": "1. febrúar 2005", "dayMonth": "1. feb", "year": 2005, "age2027": 22 },
-        { "name": "Sigurjón Axel", "shortName": "Sigurjón", "date": "4. febrúar 1974", "dayMonth": "4. feb", "year": 1974, "age2027": 53 },
-        { "name": "Viktor Ingi", "shortName": "Viktor", "date": "14. mars 1999", "dayMonth": "14. mar", "year": 1999, "age2027": 28 },
-        { "name": "Birkir Evan", "shortName": "Birkir", "date": "6. maí 2014", "dayMonth": "6. maí", "year": 2014, "age2027": 13 },
-        { "name": "Axel Bjarkar", "shortName": "Axel", "date": "22. júní 2003", "dayMonth": "22. jún", "year": 2003, "age2027": 24, "inSummer": True },
-        { "name": "Sara Kristín", "shortName": "Sara", "date": "18. júlí 2010", "dayMonth": "18. júl", "year": 2010, "age2027": 17, "inSummer": True },
-        { "name": "Þórhildur Soffía", "shortName": "Þórhildur", "date": "10. ágúst 2010", "dayMonth": "10. ágú", "year": 2010, "age2027": 17, "inSummer": True },
-        { "name": "Rannveig Arna", "shortName": "Rannveig", "date": "5. september 2005", "dayMonth": "5. sep", "year": 2005, "age2027": 22 }
+        { "name": "Ólafía Rósbjörg (Lóa)", "shortName": "Lóa", "avatar": "images/avatars/loa.jpg", "date": "10. janúar 1974", "dayMonth": "10. jan", "year": 1974, "age2027": 53 },
+        { "name": "Vala Björk", "shortName": "Vala", "avatar": "images/avatars/vala.jpg", "date": "1. febrúar 2005", "dayMonth": "1. feb", "year": 2005, "age2027": 22 },
+        { "name": "Sigurjón Axel", "shortName": "Sigurjón", "avatar": "images/avatars/sigurjon.jpg", "date": "4. febrúar 1974", "dayMonth": "4. feb", "year": 1974, "age2027": 53 },
+        { "name": "Viktor Ingi", "shortName": "Viktor", "avatar": "images/avatars/viktor.jpg", "date": "14. mars 1999", "dayMonth": "14. mar", "year": 1999, "age2027": 28 },
+        { "name": "Birkir Evan", "shortName": "Birkir", "avatar": "images/avatars/birkir.jpg", "date": "6. maí 2014", "dayMonth": "6. maí", "year": 2014, "age2027": 13 },
+        { "name": "Axel Bjarkar", "shortName": "Axel", "avatar": "images/avatars/axel.jpg", "date": "22. júní 2003", "dayMonth": "22. jún", "year": 2003, "age2027": 24, "inSummer": True },
+        { "name": "Sara Kristín", "shortName": "Sara", "avatar": "images/avatars/sara.jpg", "date": "18. júlí 2010", "dayMonth": "18. júl", "year": 2010, "age2027": 17, "inSummer": True },
+        { "name": "Þórhildur Soffía", "shortName": "Þórhildur", "avatar": "images/avatars/thorhildur.jpg", "date": "10. ágúst 2010", "dayMonth": "10. ágú", "year": 2010, "age2027": 17, "inSummer": True },
+        { "name": "Rannveig Arna", "shortName": "Rannveig", "avatar": "images/avatars/rannveig.jpg", "date": "5. september 2005", "dayMonth": "5. sep", "year": 2005, "age2027": 22 }
     ]
 
     weeks = [
